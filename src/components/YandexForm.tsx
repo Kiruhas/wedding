16be978c7c7aaad933f0aab04d3e516093599fda
Kirill Ovchinnikov <kirill.ovchinnikov@norbit.ru>
2026-03-21 14:05:39 +0300

    feat: remove modal

--- src/components/YandexForm.tsx
+++ src/components/YandexForm.tsx
@@ -1,25 +1,6 @@
-import { Box, Button, IconButton, Typography } from '@mui/material'
-import { useState } from 'react'
-import ReactModal from 'react-modal'
-import CloseIcon from '@mui/icons-material/Close'
+import { Box, Button, Typography } from '@mui/material'
 
 const YandexForm = () => {
-	const [isOpen, setIsOpen] = useState(false)
-
-	const customStyles = {
-		content: {
-			inset: '20px',
-			padding: '10px'
-		}
-	}
-
-	const openModal = () => {
-		setIsOpen(true)
-	}
-	const closeModal = () => {
-		setIsOpen(false)
-	}
-
 	return (
 		<Box
 			component='section'
@@ -63,37 +44,14 @@ const YandexForm = () => {
 			</Typography>
 			<Button
 				variant='contained'
-				onClick={openModal}
+				href='https://forms.yandex.ru/u/69bcdf94eb6146541f4db1d2'
 				sx={{
 					my: 4
 				}}
+				target='_blank'
 			>
 				Пройти опрос
 			</Button>
-			<ReactModal
-				isOpen={isOpen}
-				onRequestClose={closeModal}
-				style={customStyles}
-				ariaHideApp={false}
-			>
-				<IconButton
-					size='small'
-					style={{
-						position: 'absolute',
-						top: 12,
-						right: 12
-					}}
-					onClick={closeModal}
-				>
-					<CloseIcon fontSize='medium' />
-				</IconButton>
-
-				<iframe
-					src='https://forms.yandex.ru/u/69bcdf94eb6146541f4db1d2?iframe=1'
-					name='ya-form-69bcdf94eb6146541f4db1d2'
-					width='100%'
-				></iframe>
-			</ReactModal>
 		</Box>
 	)
 }
